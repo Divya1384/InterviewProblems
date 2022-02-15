1d588d3120623de80b4e8f476af9a1cf6a16151f
Haridas <divya.haridas@philips.com>
2022-02-15 17:08:30 +0530

    Merge Sorted Arrays problem

---
+++ MergeSortedArrays/MergeArrays.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSortedArrays
+{
+    public class MergeArrays
+    {
+        public void Merge(ref int[] firstArray, ref int[] secondArray)
+        {
+            int firstArrayIndex = 0, secondArrayIndex = 0, mergeArrayIndex = 0;
+
+            int firstArraySize = firstArray.Length;
+            int secondArraySize = secondArray.Length;
+
+            int highestFirstArrayElement = firstArray[firstArraySize - 1];
+            int highestSecondArrayElement = secondArray[secondArraySize - 1];
+
+            int maxValue = (highestFirstArrayElement > highestSecondArrayElement)
+                ? highestFirstArrayElement + 1
+                : highestSecondArrayElement + 1;
+
+            while (firstArrayIndex < firstArraySize && secondArrayIndex < secondArraySize &&
+                   mergeArrayIndex < (firstArraySize + secondArraySize))
+            {
+                int firstArrayElement = firstArray[firstArrayIndex] % maxValue;
+                int secondArrayElement = secondArray[secondArrayIndex] % maxValue;
+
+                if (firstArrayElement <= secondArrayElement)
+                {
+                    if (mergeArrayIndex < firstArraySize)
+                    {
+                        firstArray[mergeArrayIndex] += firstArrayElement * maxValue;
+                    }
+                    else
+                    {
+                        secondArray[mergeArrayIndex - firstArraySize] += firstArrayElement * maxValue;
+                    }
+
+                    mergeArrayIndex++;
+                    firstArrayIndex++;
+                }
+                else
+                {
+                    if (mergeArrayIndex < firstArraySize)
+                    {
+                        firstArray[mergeArrayIndex] += secondArrayElement * maxValue;
+                    }
+                    else
+                    {
+                        secondArray[mergeArrayIndex - firstArraySize] += secondArrayElement * maxValue;
+                    }
+
+                    mergeArrayIndex++;
+                    secondArrayIndex++;
+                }
+            }
+
+            while (firstArrayIndex < firstArraySize)
+            {
+                int firstArrayElement = firstArray[firstArrayIndex] % maxValue;
+
+                if (mergeArrayIndex < firstArraySize)
+                {
+                    firstArray[mergeArrayIndex] += firstArrayElement * maxValue;
+                }
+                else
+                {
+                    secondArray[mergeArrayIndex - firstArraySize] += firstArrayElement * maxValue;
+                }
+
+                mergeArrayIndex++;
+                firstArrayIndex++;
+            }
+
+            while (secondArrayIndex < secondArraySize)
+            {
+                int secondArrayElement = secondArray[secondArrayIndex] % maxValue;
+
+                if (mergeArrayIndex < firstArraySize)
+                {
+                    firstArray[mergeArrayIndex] += secondArrayElement * maxValue;
+                }
+                else
+                {
+                    secondArray[mergeArrayIndex - firstArraySize] += secondArrayElement * maxValue;
+                }
+
+                secondArrayIndex++;
+                mergeArrayIndex++;
+            }
+
+            for (int index = 0; index < firstArraySize; index++)
+            {
+                firstArray[index] = firstArray[index] / maxValue;
+            }
+
+            for (int index = 0; index < secondArraySize; index++)
+            {
+                secondArray[index] = secondArray[index] / maxValue;
+            }
+        }
+    }
+}
---
+++ MergeSortedArrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSortedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] firstArray = { 10, 13 };
+            //int[] secondArray = { 2, 3 };
+
+            int[] firstArray = { 1, 5, 9, 10, 15, 20 };
+            int[] secondArray = { 2, 3, 8, 13 };
+
+            //int[] firstArray = { 1, 3, 5, 7, 9 };
+            //int[] secondArray = { 0, 2, 4, 6, 8, 10 };
+
+            MergeArrays objMergeArrays = new MergeArrays();
+            objMergeArrays.Merge(ref firstArray, ref secondArray);
+
+            for (int index = 0; index < firstArray.Length; index++)
+            {
+                Console.Write($"{firstArray[index]} ");
+            }
+
+            Console.WriteLine();
+
+            for (int index = 0; index < secondArray.Length; index++)
+            {
+                Console.Write($"{secondArray[index]} ");
+            }
+
+            Console.Read();
+        }
+    }
+}
